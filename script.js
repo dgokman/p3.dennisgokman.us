b893966347e1d2e56b55bef744d3387d7b073965
Dennis Gokman <dennis.gokman@gmail.com>
2013-11-21 21:58:09 -0500

    external js

--- script.js
+++ script.js
@@ -26,3 +26,183 @@ $( "#f" )
   .on( "mousedown", function() {
     document.getElementById('f-mp3').play();
   });
+$( "#g" )
+  .on( "mousedown", function() {
+    document.getElementById('g-mp3').play();
+  });
+$( "#a" )
+  .on( "mousedown", function() {
+    document.getElementById('a-mp3').play();
+  });
+$( "#b" )
+  .on( "mousedown", function() {
+    document.getElementById('b-mp3').play();
+  });
+$( "#c1" )
+  .on( "mousedown", function() {
+    document.getElementById('c1-mp3').play();
+  });
+$( "#d1" )
+  .on( "mousedown", function() {
+    document.getElementById('d1-mp3').play();
+  });
+$( "#e1" )
+  .on( "mousedown", function() {
+    document.getElementById('e1-mp3').play();
+  });
+$( "#f1" )
+  .on( "mousedown", function() {
+    document.getElementById('f1-mp3').play();
+  });
+$( "#g1" )
+  .on( "mousedown", function() {
+    document.getElementById('g1-mp3').play();
+  });
+$( "#a1" )
+  .on( "mousedown", function() {
+    document.getElementById('a1-mp3').play();
+  });
+$( "#b1" )
+  .on( "mousedown", function() {
+    document.getElementById('b1-mp3').play();
+  });
+$( "#c2" )
+  .on( "mousedown", function() {
+    document.getElementById('c2-mp3').play();
+  });
+$( "#d2" )
+  .on( "mousedown", function() {
+    document.getElementById('d2-mp3').play();
+  });
+$( "#e2" )
+  .on( "mousedown", function() {
+    document.getElementById('e2-mp3').play();
+  });
+$( "#f2" )
+  .on( "mousedown", function() {
+    document.getElementById('f2-mp3').play();
+  });
+$( "#g2" )
+  .on( "mousedown", function() {
+    document.getElementById('g2-mp3').play();
+  });
+$( "#a2" )
+  .on( "mousedown", function() {
+    document.getElementById('a2-mp3').play();
+  });
+$( "#b2" )
+  .on( "mousedown", function() {
+    document.getElementById('b2-mp3').play();
+  });
+$( "#c3" )
+  .on( "mousedown", function() {
+    document.getElementById('c3-mp3').play();
+  });
+$( "#d3" )
+  .on( "mousedown", function() {
+    document.getElementById('d3-mp3').play();
+  });
+$( "#e3" )
+  .on( "mousedown", function() {
+    document.getElementById('e3-mp3').play();
+  });
+$( "#f3" )
+  .on( "mousedown", function() {
+    document.getElementById('f3-mp3').play();
+  });
+$( "#g3" )
+  .on( "mousedown", function() {
+    document.getElementById('g3-mp3').play();
+  });
+$( "#a3" )
+  .on( "mousedown", function() {
+    document.getElementById('a3-mp3').play();
+  });
+$( "#b3" )
+  .on( "mousedown", function() {
+    document.getElementById('b3-mp3').play();
+  });
+$( "#c4" )
+  .on( "mousedown", function() {
+    document.getElementById('c4-mp3').play();
+  });
+$( "#c-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('c-sh-mp3').play();
+  });
+$( "#d-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('d-sh-mp3').play();
+  });
+$( "#f-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('f-sh-mp3').play();
+  });
+$( "#g-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('g-sh-mp3').play();
+  });
+$( "#a-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('a-sh-mp3').play();
+  });
+$( "#c1-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('c1-sh-mp3').play();
+  });
+$( "#d1-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('d1-sh-mp3').play();
+  });
+$( "#f1-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('f1-sh-mp3').play();
+  });
+$( "#g1-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('g1-sh-mp3').play();
+  });
+$( "#a1-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('a1-sh-mp3').play();
+  });
+$( "#c2-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('c2-sh-mp3').play();
+  });
+$( "#d2-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('d2-sh-mp3').play();
+  });
+$( "#f2-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('f2-sh-mp3').play();
+  });
+$( "#g2-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('g2-sh-mp3').play();
+  });
+$( "#a2-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('a2-sh-mp3').play();
+  });
+$( "#c3-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('c3-sh-mp3').play();
+  });
+$( "#d3-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('d3-sh-mp3').play();
+  });
+$( "#f3-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('f3-sh-mp3').play();
+  });
+$( "#g3-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('g3-sh-mp3').play();
+  });
+$( "#a3-sh" )
+  .on( "mousedown", function() {
+    document.getElementById('a3-sh-mp3').play();
+  });
